10bc92d9fdccb0b5a647e538803b2e12077579df
Churtified <88999504+Churtified@users.noreply.github.com>
2022-06-29 14:12:11 +0930

    added event backgroundColor change

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
 
-for (i = 0; i < 256; i++) {
+for (i = 1; i < 257; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
+    grid.textContent = i;
     container.appendChild(grid);
 }
+
+container.addEventListener('mouseover', function(e) {
+    e.target.style.backgroundColor = 'blue';
+});
