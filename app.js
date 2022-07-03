ee28664d14e32f5148a8b69424fbaf04142009bd
Churtified <88999504+Churtified@users.noreply.github.com>
2022-07-03 14:54:23 +0930

    redo all

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
 const container = document.querySelector('.container');
-const grid = document.querySelector('.grid');
+let block;
+let target;
 
-for (i = 1; i < 257; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.textContent = i;
-    container.appendChild(grid);
+function promptSize() {
+    let num = prompt("Choose a grid size (between 5-64)!", 16);
+    if (num >= 5 && num <= 64) {
+        makeGrid(num, num);
+    }
+    else {
+        do {
+            num = prompt("Invalid grid size, try again - between 5-64")
+        }
+        while (num < 5 || num > 64);
+        makeGrid(num, num);
+    }
 }
 
-container.addEventListener('mouseover', function(e) {
-    e.target.style.backgroundColor = 'blue';
+function makeGrid(numRow, numCol) {
+    container.style.setProperty('--numRows', numRow);
+    container.style.setProperty('--numCols', numCol);
+    for (let i = 0; i < numRow; i++) {
+        makeBlock(numCol);
+    }
+}
+
+function makeBlock(numBlock) {
+    for (let i = 0; i < numBlock; i++) {
+        block = document.createElement('div');
+        block.classList.add('block');
+        container.appendChild(block);
+    }
+}
+
+//effect
+
+function trail(target) {
+    target.style.backgroundColor = 'black';
+}
+
+container.addEventListener("mouseover", function (e) {
+    target = e.target;
+
+    if (target.matches("div.block")) {
+        trail(target);
+    }
 });
+
+const reset = document.querySelector('#resetBtn');
+reset.addEventListener("click", function() {
+    window.location.reload();
+});
+
+promptSize();
